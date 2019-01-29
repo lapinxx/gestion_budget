8448c4244299490e2fa5c194a1c52b2a04f08541
lapinxx <jean.konczak@gmail.com>
2019-01-29 13:09:15 +0100

    //commit de test

--- fuel/app/classes/presenter/appli/smensuel.php
+++ fuel/app/classes/presenter/appli/smensuel.php
@@ -1,5 +1,5 @@
 <?php
-
+//test commit
 class Presenter_Appli_Smensuel extends Presenter {
 
     public $month = null;
---
+++ fuel/app/logs/2019/01/29.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2019-01-29 13:06:07 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
