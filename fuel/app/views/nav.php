e6e8320935a2babbcb48fad9b6ccbcac30c5f3e1
lapinxx <jean.konczak@gmail.com>
2019-01-31 13:09:39 +0100

    refacto du nav :)

--- fuel/app/views/nav.php
+++ fuel/app/views/nav.php
@@ -1,20 +1,15 @@
 
-<?php
-//code pour préparer les ul li nav :) 
-$items = array(
-    Html::anchor(Uri::create('appli/smensuel'), 'Accueil'),
-    Html::anchor(Uri::create('appli/smensuel'), 'Suivi Mensuel'),
-    Html::anchor(Uri::create('appli/smensuel'), 'Suivi Professionel'),
-    Html::anchor(Uri::create('appli/smensuel'), 'Statistique'),
-    Html::anchor(Uri::create('appli/smensuel'), 'Import'),
-    Html::anchor(Uri::create('appli/smensuel'), 'Saisie manuel'),
-    Html::anchor(Uri::create('appli/smensuel'), 'Profil'),
-    Html::anchor(Uri::create('appli/smensuel'), 'Paramétre')
-);
-?>
-
 <nav>
     <?php
-    echo Html::ul($items); // code pour l'afficher
+    echo Html::ul([
+        Html::anchor(Uri::create('appli/smensuel'), 'Accueil'),
+        Html::anchor(Uri::create('appli/smensuel'), 'Suivi Mensuel'),
+        Html::anchor(Uri::create('appli/smensuel'), 'Suivi Professionel'),
+        Html::anchor(Uri::create('appli/smensuel'), 'Statistique'),
+        Html::anchor(Uri::create('appli/smensuel'), 'Import'),
+        Html::anchor(Uri::create('appli/smensuel'), 'Saisie manuel'),
+        Html::anchor(Uri::create('appli/smensuel'), 'Profil'),
+        Html::anchor(Uri::create('appli/smensuel'), 'Paramétre'),
+    ]); // code pour l'afficher
     ?>
 </nav>
